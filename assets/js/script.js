1e736025e47c35434ff4ff03a844be8cc0fc7df0
Jose <joseluissalgadov02@gmail.com>
2025-06-17 00:59:32 +0200

    Sección de tres columnas mejoradas y click en menú movil

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,16 @@
 const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
 const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+
+// Para colapsar el navbar (en versión móvil) al hacer click en un link
+document.addEventListener("DOMContentLoaded", function () {
+    var navLinks = document.querySelectorAll('.navbar-collapse .nav-link');
+    var navbarCollapse = document.querySelector('.navbar-collapse');
+    navLinks.forEach(function(link) {
+      link.addEventListener('click', function () {
+        if (navbarCollapse.classList.contains('show')) {
+          var collapse = new bootstrap.Collapse(navbarCollapse, {toggle: false});
+          collapse.hide();
+        }
+      });
+    });
+  });
