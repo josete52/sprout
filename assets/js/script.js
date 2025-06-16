2957f56c3c853e218466c26189a58a8a356ca5b2
Jose <joseluissalgadov02@gmail.com>
2025-06-17 00:26:53 +0200

    Renombrado de imágenes y efectos en texto de bienvenida

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,2 @@
 const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
 const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
-
-
-$(document).ready(function() { 
-    const photo = $(".marca"); 
-    $(photo).on("mouseover", function() {
-       $(this).addClass("hoverimg"); 
-    }).on("mouseleave", function() {
-       $(this).removeClass("hoverimg"); 
-    }); 
-});
